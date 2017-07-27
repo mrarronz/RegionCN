a3f0f25a3b62653af2d754b37d389d2beda580a9
mrarronz <zhuhao1340@gmail.com>
2017-07-27 17:17:33 +0800

    update code

--- RegionCN/RegionHelper.swift
+++ RegionCN/RegionHelper.swift
@@ -8,44 +8,44 @@
 import UIKit
 import MJExtension
 
-class District: NSObject {
-    var districtId: String?
-    var districtName: String?
+public class District: NSObject {
+    public var districtId: String?
+    public var districtName: String?
     
-    override static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
+    override public static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
         return ["districtId" : "_id", "districtName" : "_name"]
     }
 }
 
-class City: NSObject {
-    var cityId: String?
-    var cityName: String?
-    var district: Any?
+public class City: NSObject {
+    public var cityId: String?
+    public var cityName: String?
+    public var district: Any?
     
-    override static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
+    override public static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
         return ["cityId" : "_id", "cityName" : "_name"]
     }
 }
 
-class Province: NSObject {
-    var provinceId: String?
-    var provinceName: String?
-    var city: NSArray?
+public class Province: NSObject {
+    public var provinceId: String?
+    public var provinceName: String?
+    public var city: NSArray?
     
-    override static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
+    override public static func mj_replacedKeyFromPropertyName() -> [AnyHashable : Any]! {
         return ["provinceId" : "_id", "provinceName" : "_name"]
     }
     
-    override static func mj_objectClassInArray() -> [AnyHashable : Any]! {
+    override public static func mj_objectClassInArray() -> [AnyHashable : Any]! {
         return ["city" : "City"]
     }
 }
 
-class RegionHelper: NSObject {
+public class RegionHelper: NSObject {
 
-    static let shared = RegionHelper()
+    public static let shared = RegionHelper()
     
-    var provincesXMLArray: NSMutableArray {
+    public var provincesXMLArray: NSMutableArray {
         let bundle = Bundle.init(for: self.classForCoder)
         let path = bundle.path(forResource: "regions", ofType: "xml")
         let xmlData = NSData.init(contentsOfFile: path!)
@@ -55,7 +55,7 @@ class RegionHelper: NSObject {
         return provinces!
     }
     
-    var regionTXTData: Array<String> {
+    private var regionTXTData: Array<String> {
         let bundle = Bundle.init(for: self.classForCoder)
         let filePath = bundle.path(forResource: "region", ofType: "txt")
         let regionString = try! NSString.init(contentsOfFile: filePath!, encoding: String.Encoding.utf8.rawValue)
@@ -63,7 +63,7 @@ class RegionHelper: NSObject {
         return array
     }
     
-    var regionTXTArray: Array<Dictionary<String, String>> {
+    public var regionTXTArray: Array<Dictionary<String, String>> {
         
         var array = Array<Dictionary<String, String>>()
         
@@ -80,7 +80,7 @@ class RegionHelper: NSObject {
     }
     
     /// 根据地区名字查找地区对应的数据字典
-    func findRegionByName(regionName: String) -> Dictionary<String, String> {
+    public func findRegionByName(regionName: String) -> Dictionary<String, String> {
         var dict = Dictionary<String, String>()
         for item in self.regionTXTArray {
             let name: String = item["regionName"]!
@@ -93,7 +93,7 @@ class RegionHelper: NSObject {
     }
     
     /// 根据地区编码查找对应的数据字典
-    func findRegionByCode(regionCode: String) -> Dictionary<String, String> {
+    public func findRegionByCode(regionCode: String) -> Dictionary<String, String> {
         var dict = Dictionary<String, String>()
         for item in self.regionTXTArray {
             let code: String = item["regionCode"]!
@@ -106,7 +106,7 @@ class RegionHelper: NSObject {
     }
     
     /// 查找中国所有省份
-    func allProvinces() -> NSArray {
+    public func allProvinces() -> NSArray {
         let defaultCountryCode = "100000"
         let suffix = "0000"
         let predicate = NSPredicate.init(format: "regionCode != %@ AND regionCode ENDSWITH[cd] %@", defaultCountryCode, suffix)
@@ -115,7 +115,7 @@ class RegionHelper: NSObject {
     }
     
     /// 根据省份编码查找该省份下所有的市区县
-    func regionsInProvince(provinceCode: String) -> NSArray {
+    public func regionsInProvince(provinceCode: String) -> NSArray {
         let prefix = provinceCode.substring(to: provinceCode.index(provinceCode.startIndex, offsetBy: 2))
         let predicate = NSPredicate.init(format: "regionCode != %@ AND regionCode BEGINSWITH[cd] %@", provinceCode, prefix)
         let results: NSArray = (self.regionTXTArray as NSArray).filtered(using: predicate) as NSArray
@@ -123,7 +123,7 @@ class RegionHelper: NSObject {
     }
     
     /// 根据省份代码和最后两个数字00查找这个省份下面的所有城市
-    func allCitiesInProvince(provinceCode: String) -> NSArray {
+    public func allCitiesInProvince(provinceCode: String) -> NSArray {
         let prefix = provinceCode.substring(to: provinceCode.index(provinceCode.startIndex, offsetBy: 2))
         let suffix = "00"
         let predicate = NSPredicate.init(format: "regionCode != %@ AND regionCode BEGINSWITH[cd] %@ AND regionCode ENDSWITH[cd] %@", provinceCode, prefix, suffix)
@@ -132,7 +132,7 @@ class RegionHelper: NSObject {
     }
     
     /// 根据城市编码查找这个城市下面所有的区县
-    func allAreasInCity(cityCode: String) -> NSArray {
+    public func allAreasInCity(cityCode: String) -> NSArray {
         let prefix = cityCode.substring(to: cityCode.index(cityCode.startIndex, offsetBy: 4))
         let predicate = NSPredicate.init(format: "regionCode != %@ AND regionCode BEGINSWITH[cd] %@", cityCode, prefix)
         let results: NSArray = (self.regionTXTArray as NSArray).filtered(using: predicate) as NSArray
